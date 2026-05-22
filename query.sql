387441987b6cfc6ff9b6c1c01a67e033db5e731e
Depthi V Somapur <depthisomapur.mca@gmail.com>
2026-05-22 15:03:25 +0530

    updated README and query file

--- query.sql
+++ query.sql
@@ -1,1 +1,55 @@
 SELECT * FROM phonebook.users;
+
+use phonebook;
+
+DROP TABLE users;
+
+CREATE TABLE users (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    username VARCHAR(100) UNIQUE NOT NULL,
+
+    password VARCHAR(255) NOT NULL
+
+);
+
+
+DROP TABLE contacts;
+
+DROP TABLE users;
+
+CREATE TABLE users (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    username VARCHAR(100) UNIQUE NOT NULL,
+
+    password VARCHAR(255) NOT NULL
+
+);
+
+CREATE TABLE contacts (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    name VARCHAR(100) NOT NULL,
+
+    phone VARCHAR(15) UNIQUE,
+
+    email VARCHAR(100),
+
+    company VARCHAR(100),
+
+    address TEXT,
+
+    category VARCHAR(50),
+
+    favorite BOOLEAN DEFAULT FALSE,
+
+    user_id INT,
+
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+
+);
